21cb2c7e0b62d4564e1d7b85c3cbd0ebbc6f5af3
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-11-25 14:45:30 +0545

    Implmented node, linkedList class function implemented append, prepend, size, head, tail, at(index), contains, find, toString

--- script.js
+++ script.js
@@ -1,1 +1,145 @@
-console.log('hi')
+class Node {
+    constructor(prop) {
+        this.prop = prop;
+        this.nextNode = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+}
+
+//Initialization
+let ls = new LinkedList(10);
+
+//Append to last 
+append(20);
+
+//Prepend to first
+prepend(0);
+
+
+//size head tail
+    // console.log(size());
+    // console.log(head());
+    // console.log(tail());
+
+
+//At Index
+atIndex(1);
+
+//pop remaining
+
+//contains(value)
+valueExist(0);
+
+
+//find(value) : index of node containing value
+console.log(findIndex(0, 20, ls.head));
+
+//toString
+toString();
+
+
+
+
+
+
+function append(value) {
+    let newNode = new Node(value);
+    ls.head.nextNode = newNode;
+    ls.tail = newNode;
+    ls.length++;
+}
+
+function prepend(value) {
+    let newNode = new Node(value);
+    newNode.nextNode = ls.head;
+    ls.head = newNode;
+    ls.length++;
+}
+
+function size(){
+    return ls.length;
+}
+
+function head(){
+    return ls.head;
+}
+
+function tail(){
+    return ls.tail;
+}
+
+function atIndex(index){
+    let myIndex = 0;
+    let node = getNode(index, myIndex, ls.head);
+    console.log(node)
+}
+
+
+function valueExist(search) {
+    console.log(searchItem(search, ls.head))
+}
+
+function getNode(index, myIndex, node ) {
+    if(node === null){
+        console.log("Index not found")
+        return node;
+    }
+
+    if(index === myIndex){
+        return node;
+    }
+
+    myIndex++;
+    return getNode(index, myIndex, node.nextNode)
+}
+
+function findIndex(index, value, node) {
+
+    if(!node){
+        return node;
+    }
+
+    if(node.prop === value){
+        return index;
+    }
+
+    index++;
+
+    return findIndex(index, value, node.nextNode);
+    
+}
+function searchItem(search, node ) {
+    if(node === null){
+        return false;
+    }
+
+    if(node.prop === search){
+        return true;
+    }
+
+    return searchItem(search, node.nextNode)
+}
+
+function toString(){
+    if(ls.length === 1){
+        console.log(ls.head.prop)
+    }
+    else{
+        console.log(ls.head.prop + concatinateValues(ls.head.nextNode))
+    }
+
+}
+
+
+function concatinateValues(node){
+    if(!node) return ' -> null';
+    return ' -> ' + node.prop + concatinateValues(node.nextNode);
+}
